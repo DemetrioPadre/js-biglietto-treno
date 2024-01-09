4d8240d52cedb2d5593c443eed90841d8ca19d16
DemetrioPadre <demetriopadre@gmail.com>
2024-01-09 15:05:03 +0100

    age 1 e 2

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+//Chiedere all utente i km da percorrere
+const kmNeeded = parseInt(prompt('numero di km'));
+console.log(kmNeeded);
+
+// //Chiedere all utente la sua età
+// const age1 = parseInt(prompt('età < 18'));
+// console.log(age1);
+
+// const age2 = parseInt(prompt('età > 65'));
+
+//Costo km al cent
+const cent = prompt(0.21);
+console.log(cent);
+
+
+//prezzo del biglietto
+const ticket = prompt(kmNeeded * cent);
+console.log(ticket);
